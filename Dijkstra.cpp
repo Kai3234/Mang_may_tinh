188215ab9e632175e6706d5ad2b9cda41efe8322
Kai3234 <khai2892005@gmail.com>
2025-03-18 14:40:44 +0700

    Create Dijkstra.cpp

---
+++ Dijkstra.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+  //Số điểm
+  int numOfPoint;
+
+  //Độ dài các cạnh giữa 2 điểm
+  vector<vector<float>> distance(numOfPoint, vector<float>(numOfPoint, 0.0));
+
+
+
+
+  //Thuật toán Dijkstra
+  //Điểm bắt đầu
+  int start;
+
+  //Điểm cuối
+  int end;
+}
